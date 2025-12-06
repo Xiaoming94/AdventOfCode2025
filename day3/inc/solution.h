bd0a53772a1bb8b463fc90f58a623249c83207d0
Xiaoming <mail@zyming.me>
2025-12-06 22:01:20 +0100

    [day3] amend clang-format to indent namespaces

--- day3/inc/solution.h
+++ day3/inc/solution.h
@@ -1,6 +1,8 @@
 #include <cstdint>
 #include <string_view>
 
-namespace solution {
-uint32_t findTotalMaxJoltage(std::string_view);
-}
+namespace solution
+{
+  uint32_t findTotalMaxJoltage(std::string_view);
+
+}  // namespace solution
