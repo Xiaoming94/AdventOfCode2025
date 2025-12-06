bd21ad964e3f4447db18b7ba6c18c28f3a10fbc7
Xiaoming <mail@zyming.me>
2025-12-06 01:45:32 +0100

    [day3] add clang-format

--- day3/src/solution.cpp
+++ day3/src/solution.cpp
@@ -1,24 +1,29 @@
 #include "solution.h"
+
 #include <algorithm>
 #include <cstdint>
 #include <ranges>
 #include <vector>
 
-namespace solution {
+namespace solution
+{
 
-std::vector<uint32_t> toIntegers(std::span<const char> batterBank) {
-  return batterBank | std::views::transform([](const char val) {
-           return static_cast<uint32_t>(val - '0');
-         }) |
+std::vector<uint32_t> toIntegers(std::span<const char> batterBank)
+{
+  return batterBank |
+         std::views::transform([](const char val) { return static_cast<uint32_t>(val - '0'); }) |
          std::ranges::to<std::vector>();
 }
 
-uint32_t findMaxJoltage(const std::vector<uint32_t> &batteryBank) {
+uint32_t findMaxJoltage(const std::vector<uint32_t>& batteryBank)
+{
   auto numberOfBatteries = batteryBank.size();
   uint32_t currentLargestJoltage = 0u;
-  for (const auto &[index, batPower1] : std::views::zip(
-           std::views::iota(size_t{1}, numberOfBatteries + 1u), batteryBank)) {
-    for (auto batPower2 : batteryBank | std::views::drop(index)) {
+  for (const auto& [index, batPower1] :
+       std::views::zip(std::views::iota(size_t{1}, numberOfBatteries + 1u), batteryBank))
+  {
+    for (auto batPower2 : batteryBank | std::views::drop(index))
+    {
       auto currentJoltage = batPower1 * 10u + batPower2;
       currentLargestJoltage = std::max(currentLargestJoltage, currentJoltage);
     }
@@ -27,10 +32,11 @@ uint32_t findMaxJoltage(const std::vector<uint32_t> &batteryBank) {
   return currentLargestJoltage;
 }
 
-uint32_t findTotalMaxJoltage(std::string_view batteryBanks) {
+uint32_t findTotalMaxJoltage(std::string_view batteryBanks)
+{
   return std::ranges::fold_left(std::views::split(batteryBanks, '\n') |
                                     std::views::transform(toIntegers) |
                                     std::views::transform(findMaxJoltage),
                                 0, std::plus{});
 }
-} // namespace solution
+}  // namespace solution
