2b4f8f2707333b26a38206dc9b97da1dc2060cfa
Xiaoming <mail@zyming.me>
2025-12-09 00:15:44 +0100

    [day5] finished day 5

--- day5/main.py
+++ day5/main.py
@@ -9,3 +9,4 @@ input = reduce(operator.add, sys.stdin, "").rstrip('\n')
 
 print("input is: %s" % input)
 print("Solution for problem 1: %d" % solution.solve_problem1(input))
+print("Solution for problem 2: %d" % solution.solve_problem2(input))
