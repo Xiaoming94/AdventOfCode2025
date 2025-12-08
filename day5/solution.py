c75a7d5f00d8269ac8b3329ecf161ecb44be3c0e
Xiaoming <mail@zyming.me>
2025-12-08 22:45:29 +0100

    [day5] Complete python solution for day 5 example

--- day5/solution.py
+++ day5/solution.py
@@ -1,3 +1,22 @@
+def create_ranges(ranges_str: str) -> set[int]:
+    def create_range(line: str) -> set[int]:
+        [lower_str, upper_str] = line.split('-')
+        upper = int(upper_str) + 1
+        lower = int(lower_str)
+        return set(range(lower,upper))
+
+    ranges = [create_range(line) for line in ranges_str.splitlines()]
+    return set.union(*ranges)
+
+def transform_to_int_ids(ingredient_liststr: str) -> list[int]:
+    return [int(ingredient_id) for ingredient_id in ingredient_liststr.split('\n')]
+
+def find_fresh_ingredients(fresh_ids: set[int], ingredients_ids: list[int]) -> int:
+    return sum(1 for id in ingredients_ids if id in fresh_ids)
+
 def solve_problem1(input: str):
-    print("input is:\n%s" % input)
-    return 0
+    [ranges_str, ingredient_liststr] = input.split('\n\n')
+    ranges = create_ranges(ranges_str)
+    ingredient_ids = transform_to_int_ids(ingredient_liststr)
+
+    return find_fresh_ingredients(ranges, ingredient_ids)
