983c96c6fe35246091dd7fdf8b5575f487d26b61
Xiaoming <mail@zyming.me>
2025-12-25 13:45:29 +0100

    [day7] create java17 project for AdventOfCode2025 Day7

---
+++ day7/app/src/main/java/org/aocday7/Main.java
@@ -0,0 +1,7 @@
+package org.aocday7;
+
+public class Main {
+    public static void main(String [] args) {
+        System.out.println(String.format("Hello World, %s", "Mr Ming"));
+    }
+}
---
+++ day7/app/src/test/java/org/aocday7/AocTest.java
@@ -0,0 +1,14 @@
+/**
+ * MAIN test file for the Advent of code
+ */
+
+package org.aocday7;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AocTest {
+    @Test void problem1Solution() {
+        System.out.println("Testing");
+    }
+}
