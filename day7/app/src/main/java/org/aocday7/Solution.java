ce6ea44a5107cfb839a5c55e9a6eacab4cdc36ed
Xiaoming <mail@zyming.me>
2025-12-30 20:13:20 +0100

    [day7] Map Parser created

--- day7/app/src/main/java/org/aocday7/Solution.java
+++ day7/app/src/main/java/org/aocday7/Solution.java
@@ -4,10 +4,68 @@
 
 package org.aocday7;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 public class Solution {
     public static int solveProblem1(String input) {
-        System.out.println(String.format("input is: %s", input));
+        new TachyonBeamTracer().createTachyonMap(input);
         return 0;
     }
 }
 
+record Coordinate(int x, int y) {}
+
+enum MapSymbol {
+    SPLIT,
+    DOT,
+    START,
+}
+
+class TachyonBeamTracer {
+    private MapSymbol toMapSymbol(char inputChar) throws IllegalArgumentException {
+        switch(inputChar) {
+            case '.' : return MapSymbol.DOT;
+            case '^' : return MapSymbol.SPLIT;
+            case 'S' : return MapSymbol.START;
+        };
+
+        throw new IllegalStateException(String.format("Invalid input character %s", inputChar));
+    }
+
+    public void createTachyonMap(String input) {
+        var inputLinesArr = input
+            .lines()
+            .toArray(String[]::new);
+
+        IntStream.range(0, inputLinesArr.length)
+            .forEach(i -> {
+                var line = inputLinesArr[i].toCharArray();
+                IntStream.range(0, line.length)
+                    .forEach(j -> {
+                        var parsedMapSymbol = toMapSymbol(line[j]);
+                        var coordinate = new Coordinate(j, i);
+                        if ( parsedMapSymbol == MapSymbol.START ) {
+                            startingPositions.add(coordinate);
+                        }
+                        this.tachyonBeamMap.put(coordinate, parsedMapSymbol);
+                    });
+            });
+
+    }
+
+    public MapSymbol getItemAt(int x, int y) {
+        var lookupCoordinate = new Coordinate(x, y);
+        return tachyonBeamMap.get(lookupCoordinate);
+    }
+
+    public List<Coordinate> getStartingPositions() {
+        return this.startingPositions;
+    }
+
+    private Map<Coordinate, MapSymbol> tachyonBeamMap = new HashMap<>();
+    private List<Coordinate> startingPositions = new ArrayList<>();
+}
---
+++ day7/app/src/test/java/org/aocday7/TachyonBeamMapTest.java
@@ -0,0 +1,42 @@
+/**
+ * Unit tests for the TachyonBeamMap internal type
+ */
+
+package org.aocday7;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TachyonBeamMapTest {
+    @Test
+    void parseOnly1Start() {
+        final String input = "S";
+        var myBeamTracer = new TachyonBeamTracer();
+        myBeamTracer.createTachyonMap(input);
+        assertEquals(1, myBeamTracer.getStartingPositions().size());
+    }
+
+    @Test
+    void testSimpleMap() {
+        final String input = ".S.";
+        var myBeamTracer = new TachyonBeamTracer();
+        myBeamTracer.createTachyonMap(input);
+        assertEquals(MapSymbol.DOT, myBeamTracer.getItemAt(0, 0));
+        assertEquals(MapSymbol.START, myBeamTracer.getItemAt(1, 0));
+        assertEquals(MapSymbol.DOT, myBeamTracer.getItemAt(2, 0));
+    }
+
+    @Test
+    void testMulitLineMap() {
+        final String input = """
+                .S.
+                ^.^
+                """;
+        var myBeamTracer = new TachyonBeamTracer();
+        myBeamTracer.createTachyonMap(input);
+        assertEquals(MapSymbol.START, myBeamTracer.getItemAt(1, 0));
+        assertEquals(MapSymbol.SPLIT, myBeamTracer.getItemAt(0, 1));
+        assertEquals(MapSymbol.DOT, myBeamTracer.getItemAt(0, 0));
+        assertEquals(MapSymbol.DOT, myBeamTracer.getItemAt(1, 1));
+    }
+}
