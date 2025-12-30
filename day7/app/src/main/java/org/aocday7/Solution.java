75beedc54de0546a4ccafa9d254d6934a084ea24
Xiaoming <mail@zyming.me>
2025-12-30 15:23:15 +0100

    [day7] Add Java solution structure

---
+++ day7/app/src/main/java/org/aocday7/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * Class for the solution of Advent of Code day7;
+ */
+
+package org.aocday7;
+
+public class Solution {
+    public static int solveProblem1(String input) {
+        System.out.println(String.format("input is: %s", input));
+        return 0;
+    }
+}
+
