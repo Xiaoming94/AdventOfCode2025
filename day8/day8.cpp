9881f8927ec27c4f4b24a0b10a3309d96f1d4311
Ming <mail@zyming.me>
2026-02-23 11:06:20 +0100

    [day8] Initialise Day3 project structure

---
+++ day8/day8.cpp
@@ -0,0 +1,8 @@
+#ifndef PROJECT_DAY8_MAIN
+#define PROJECT_DAY8_MAIN
+
+#include <print>
+
+int main() { std::println("Hello World"); }
+
+#endif
---
+++ day8/inc/solution.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <print>
+#include <string>
+#include <string_view>
+
+namespace solution {
+  std::string solveProblem1(std::string_view /*args*/) {
+    std::println("running problem1 solution");
+    return "";
+  }
+}  // namespace solution
