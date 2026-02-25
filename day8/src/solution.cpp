1c6823c8b2babe6328017e119eb119aec89a534b
Ming <mail@zyming.me>
2026-02-25 16:04:18 +0100

    [day8] refactoring to remove dependencies on floats

--- day8/src/solution.cpp
+++ day8/src/solution.cpp
@@ -1,44 +1,29 @@
 #include "solution.h"
 
+#include <algorithm>
 #include <cassert>
-#include <charconv>
-#include <cmath>
-#include <optional>
+#include <memory>
 #include <print>
 #include <queue>
 #include <ranges>
 #include <vector>
 
-namespace {
-  constexpr auto err = 0.001f;
-}
-
 namespace solution {
+  constexpr auto DIMENSIONS{3u};
+  namespace cviews = std::views;
+  namespace cranges = std::ranges;
   namespace internal {
     class Node {
      public:
+      using Ptr_sp = std::shared_ptr<Node>;
+      using PtrVec_t = std::vector<Ptr_sp>;
       Node(std::uint32_t x, std::uint32_t y, std::uint32_t z)
         : m_x(x)
         , m_y(y)
         , m_z(z) {}
 
-      static std::optional<Node> fromString(std::string_view nodeStr) {
-        if (nodeStr.empty()) {
-          return std::nullopt;
-        }
-        auto coordinates
-            = std::views::split(nodeStr, ',')
-              | std::views::transform([](auto&& range) {
-                  std::string_view strView(&*range.begin(), std::ranges::distance(range));
-                  std::println("parsing: {}", strView);
-                  std::uint32_t coord;
-                  std::from_chars(strView.data(), strView.data() + strView.size(), coord);
-                  return coord;
-                })
-              | std::ranges::to<std::vector<std::uint32_t>>();
-
-        assert(coordinates.size() == 3 && "Invalid coordinate arg string");
-        return Node(coordinates[0], coordinates[1], coordinates[2]);
+      static auto fromNodeData(std::vector<std::uint32_t>&& nodeData) -> Ptr_sp {
+        return std::make_shared<Node>(nodeData[0], nodeData[1], nodeData[2]);
       }
 
       std::uint32_t getX() const { return m_x; }
@@ -47,60 +32,73 @@ namespace solution {
 
       std::uint32_t getZ() const { return m_z; }
 
-      float calcDistanceTo(const Node& other) const {
-        float dx = m_x - other.getX();
-        float dy = m_y - other.getY();
-        float dz = m_z - other.getZ();
+      auto calcDistanceTo(const Node& other) const {
+        std::uint64_t dx = m_x - other.getX();
+        std::uint64_t dy = m_y - other.getY();
+        std::uint64_t dz = m_z - other.getZ();
 
         // Distance formula
-        return std::sqrt(std::pow(dx, 2.0f) + std::pow(dy, 2.0f) + std::pow(dz, 2.0f));
+        return (dx * dx) + (dy * dy) + (dz * dz);
       }
+
+      void use() { m_used = true; }
+
+      bool isUsed() const { return m_used; }
+
+      auto operator<=>(const Node&) const = default;
 
      private:
       std::uint32_t m_x;
       std::uint32_t m_y;
       std::uint32_t m_z;
+      bool m_used{false};
     };
 
     struct Edge {
-      Node node1;
-      Node node2;
-      float length;
+      Node::Ptr_sp node1;
+      Node::Ptr_sp node2;
+      std::uint64_t length;
 
-      auto operator<=>(const Edge& other) const {
-        // Check for equality using IEEE methodology
-        if (std::abs(length - other.length) < err) {
-          return std::partial_ordering::equivalent;
-        }
-        return length <=> other.length;
-      }
+      auto operator<=>(const Edge& other) const { return length <=> other.length; }
     };
 
-    using EdgeQueue_t = std::priority_queue<Edge>;
+    std::vector<std::uint32_t> parseNodeData(auto&& data) {
+      std::vector<std::uint32_t> nodeData;
+      nodeData.reserve(DIMENSIONS);
+      for (auto&& subdata : cviews::split(data, ',')) {
+        nodeData.push_back(cranges::fold_left(subdata, 0u, [](std::uint32_t acc, auto&& c) {
+          std::uint32_t val = c - '0';
+          return acc * 10 + val;
+        }));
+      }
+      if (!nodeData.empty()) {
+        assert(nodeData.size() == DIMENSIONS && "Invalid Input format");
+      }
+      return nodeData;
+    }
+
+    using EdgeQueue_t = std::priority_queue<Edge, std::vector<Edge>, std::greater<Edge>>;
     EdgeQueue_t parseTextCreateEdgeQueue(std::string_view nodeList) {
-      auto nodes = std::views::split(nodeList, '\n')
-                   | std::views::transform([](auto&& range) {
-                       return std::string_view(&*range.begin(), std::ranges::distance(range));
-                     })
-                   | std::views::transform(Node::fromString)
-                   | std::views::filter([](auto node) { return node.has_value(); })
-                   | std::views::transform([](auto node) { return node.value(); })
-                   | std::ranges::to<std::vector<Node>>();
+      auto nodeData = cviews::split(nodeList, '\n')
+                      | cviews::transform([](auto&& data) { return parseNodeData(data); })
+                      | cviews::transform(Node::fromNodeData)
+                      | cranges::to<Node::PtrVec_t>();
+
       EdgeQueue_t edges;
-      for (size_t i{0u}; i < nodes.size(); i += 1) {
-        for (size_t j{i + 1}; j < nodes.size(); j += 1) {
-          Edge edge{.node1 = nodes[i],
-                    .node2 = nodes[j],
-                    .length = nodes[i].calcDistanceTo(nodes[j])};
-          edges.push(edge);
+      for (size_t i : cviews::iota(0u, nodeData.size())) {
+        for (size_t j : cviews::iota(i + 1, nodeData.size())) {
+          edges.push(Edge{.node1 = nodeData.at(i),
+                          .node2 = nodeData.at(j),
+                          .length = nodeData[i]->calcDistanceTo(*nodeData[j])});
         }
       }
+
       return edges;
     }
 
   }  // namespace internal
   std::uint32_t solveProblem1(std::string_view input, std::uint32_t nWires) {
-    std::println("input is:\n {}", input);
+    std::println("input is:\n{}", input);
     auto edges = internal::parseTextCreateEdgeQueue(input);
     return 0;
   }
