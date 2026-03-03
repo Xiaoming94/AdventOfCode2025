1f2dcf95614100e3839e541c1f99d2094f8ecef7
Ming <mail@zyming.me>
2026-03-03 15:13:41 +0100

    [day8] refactoring solution

--- day8/src/solution.cpp
+++ day8/src/solution.cpp
@@ -63,6 +63,25 @@ namespace solution {
       using Vec_t = std::vector<Edge>;
     };
 
+    class Circuit {
+     public:
+      using Ptr_t = std::shared_ptr<Circuit>;
+      Circuit(id_t circuitId)
+        : m_id(circuitId) {}
+
+      auto getId() const { return m_id; }
+
+      auto getNodes() { return m_nodes; }
+
+      void merge(Circuit& other) { m_nodes.merge(other.getNodes()); }
+
+      auto add(id_t newNodeId) { return m_nodes.insert(newNodeId); }
+
+     private:
+      id_t m_id;
+      std::unordered_set<id_t> m_nodes;
+    };
+
     std::vector<std::uint32_t> parseNodeData(auto&& data) {
       std::vector<std::uint32_t> nodeData;
       nodeData.reserve(DIMENSIONS);
@@ -78,101 +97,83 @@ namespace solution {
       return nodeData;
     }
 
-    Edge::Vec_t createProblemDataFrom(std::string_view nodeList, std::uint32_t nWires) {
-      auto nodeData = cviews::split(nodeList, '\n')
-                      | cviews::transform([](auto&& data) { return parseNodeData(data); })
-                      | cviews::transform(Node::fromNodeData)
-                      | cranges::to<Node::PtrVec_t>();
-
-      std::priority_queue<Edge, std::vector<Edge>, std::greater<Edge>> edgesQueue;
-      for (id_t i : cviews::iota(0u, nodeData.size())) {
-        for (id_t j : cviews::iota(i + 1, nodeData.size())) {
-          auto& node1 = nodeData.at(i);
-          auto& node2 = nodeData.at(j);
-          auto length = node1->calcDistanceTo(*node2);
-          edgesQueue.push(Edge{.node1Id = i, .node2Id = j, .length = length});
-        }
-      }
-
-      Edge::Vec_t edges;
-      while (not edgesQueue.empty() && nWires > 0) {
-        const auto& edge = edgesQueue.top();
-        edges.emplace_back(edge);
-        edgesQueue.pop();
-        nWires -= 1;
-      }
-
-      return edges;
-    }
-
-    static std::uint32_t circuitIds{0};
-
-    class Circuit {
+    template <typename comparator_t>
+    class GraphBuilder {
      public:
-      using Ptr_t = std::shared_ptr<Circuit>;
-      Circuit()
-        : m_id(circuitIds++) {}
+      using EdgesQueue_t = std::priority_queue<Edge, std::vector<Edge>, comparator_t>;
+      GraphBuilder() = default;
 
-      auto getId() const { return m_id; }
+      /**
+       * @brief creates a queue of edges sorted in order specified by comparator_t.
+       *
+       *
+       */
+      static std::pair<id_t, EdgesQueue_t> createGraphEdges(std::string_view nodeList) {
+        auto nodeData = cviews::split(nodeList, '\n')
+                        | cviews::transform([](auto&& data) { return parseNodeData(data); })
+                        | cviews::transform(Node::fromNodeData)
+                        | cranges::to<Node::PtrVec_t>();
 
-      auto getNodes() { return m_nodes; }
+        std::priority_queue<Edge, std::vector<Edge>, comparator_t> edgesQueue;
+        for (id_t i : cviews::iota(0u, nodeData.size())) {
+          for (id_t j : cviews::iota(i + 1, nodeData.size())) {
+            auto& node1 = nodeData.at(i);
+            auto& node2 = nodeData.at(j);
+            auto length = node1->calcDistanceTo(*node2);
+            edgesQueue.push(Edge{.node1Id = i, .node2Id = j, .length = length});
+          }
+        }
 
-      void merge(Circuit& other) { m_nodes.merge(other.getNodes()); }
-
-      auto add(id_t newNodeId) { return m_nodes.insert(newNodeId); }
-
-      auto operator<=>(const Circuit& other) const { return m_id <=> other.getId(); }
-
-     private:
-      std::uint32_t m_id;
-      std::unordered_set<id_t> m_nodes;
+        return std::make_pair<>(nodeData.size(), edgesQueue);
+      }
     };
+
+    auto createCircuits(id_t numberOfNodes)
+        -> std::pair<std::vector<Circuit::Ptr_t>, std::unordered_map<id_t, Circuit::Ptr_t>> {
+      auto circuits = cviews::iota(0u, numberOfNodes)
+                      | cviews::transform([](auto&& id) {
+                          auto circuit = std::make_shared<Circuit>(id);
+                          circuit->add(id);
+                          return circuit;
+                        })
+                      | cranges::to<std::vector<Circuit::Ptr_t>>();
+
+      std::unordered_map<id_t, Circuit::Ptr_t> idToCircuits;
+      for (auto&& [id, circuit] : circuits | cviews::enumerate) {
+        idToCircuits[id] = circuit;
+      }
+
+      return std::make_pair<>(circuits, idToCircuits);
+    }
 
   }  // namespace internal
 
   std::uint32_t solveProblem1(std::string_view input, std::uint32_t nWires) {
-    auto edges = internal::createProblemDataFrom(input, nWires);
-    using internal::Circuit;
-    std::vector<Circuit::Ptr_t> circuits;
-    std::unordered_map<id_t, Circuit::Ptr_t> idToCircuits;
+    auto [numberOfNodes, edges] = internal::GraphBuilder<std::greater<>>().createGraphEdges(input);
+    auto [circuits, idToCircuits] = internal::createCircuits(numberOfNodes);
 
-    for (const auto& [node1Id, node2Id, _] : edges) {
-      auto node1InCircuit = idToCircuits.contains(node1Id);
-      auto node2InCircuit = idToCircuits.contains(node2Id);
+    while (not edges.empty() && nWires > 0) {
+      auto& [id1, id2, _] = edges.top();
+      auto circuit1 = idToCircuits[id1];
+      auto circuit2 = idToCircuits[id2];
 
-      // Merging two circuits
-      if (node1InCircuit and node2InCircuit) {
-        auto circuit1 = idToCircuits[node1Id];
-        auto circuit2 = idToCircuits[node2Id];
-        if (circuit1 != circuit2) {
-          for (id_t nodeId : circuit2->getNodes()) {
-            idToCircuits[nodeId] = circuit1;
-          }
-          circuit1->merge(*circuit2);
-          auto it = std::find_if(circuits.begin(), circuits.end(), [&circuit2](auto&& circuit) {
-            return circuit->getId() == circuit2->getId();
-          });
-          if (it != circuits.end()) {
-            circuits.erase(it);
-          }
-          circuit2.reset();
+      if (circuit1 != circuit2) {
+        for (id_t nodeId : circuit2->getNodes()) {
+          idToCircuits[nodeId] = circuit1;
         }
-
-      } else if (node1InCircuit) {
-        idToCircuits[node1Id]->add(node2Id);
-        idToCircuits[node2Id] = idToCircuits[node1Id];
-      } else if (node2InCircuit) {
-        idToCircuits[node2Id]->add(node1Id);
-        idToCircuits[node1Id] = idToCircuits[node2Id];
-      } else {
-        auto newCircuit = std::make_shared<Circuit>();
-        newCircuit->add(node1Id);
-        newCircuit->add(node2Id);
-        idToCircuits[node1Id] = newCircuit;
-        idToCircuits[node2Id] = newCircuit;
-        circuits.emplace_back(newCircuit);
+        circuit1->merge(*circuit2);
+        auto it = std::find_if(circuits.begin(), circuits.end(), [&circuit2](auto&& circuit) {
+          return circuit->getId() == circuit2->getId();
+        });
+        if (it != circuits.end()) {
+          circuits.erase(it);
+        }
+        circuit2.reset();
       }
+      nWires -= 1;
+      edges.pop();
     }
+
     cranges::sort(circuits, [](const auto& circuit1, const auto& circuit2) {
       return circuit1->getNodes().size() > circuit2->getNodes().size();
     });
